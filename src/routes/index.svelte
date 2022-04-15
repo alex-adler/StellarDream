fbc747637758448a5c1a2ab81cd670886da95d20
Alex Adler <project.alex.adler@gmail.com>
2022-04-15 23:32:27 +0100

    Initial Heroku commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Stellar Dream</title>
+</svelte:head>
+
+<h1>Bom dia Matteo</h1>
+
+<style lang="scss">
+    h1{
+        text-align: center;
+    }
+</style>
