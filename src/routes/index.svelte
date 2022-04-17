d373ddf1bfbb60baa04590a222239c26f9e8f413
Alex Adler <project.alex.adler@gmail.com>
2022-04-17 12:08:48 +0100

    Added a nav bar

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,29 @@
 <svelte:head>
-    <title>Stellar Dream</title>
+	<title>Stellar Dream</title>
 </svelte:head>
 
-<h1>Bom dia Matteo</h1>
+<section class="main">
+	<h1>A very real corporation</h1>
+</section>
+
+<section class="intro" />
 
 <style lang="scss">
-    h1{
-        text-align: center;
-    }
+	section {
+		height: 100vh;
+	}
+	.main {
+		background-color: rgb(16, 5, 54);
+		// background-image: url('https://images.pexels.com/photos/956981/milky-way-starry-sky-night-sky-star-956981.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+		background-position: center;
+		background-size: cover;
+		// opacity: 1;
+	}
+	h1 {
+		font-family: 'Roboto', Helvetica, Arial, sans-serif;
+		color: aliceblue;
+		text-align: center;
+		margin: 0;
+		padding-top: 20vh;
+	}
 </style>
